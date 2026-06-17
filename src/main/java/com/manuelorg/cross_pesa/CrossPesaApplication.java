5f04b47d59ce19c5a8e88c581c4b1514e5f938c0
Emmanuel Odhiambo <emanuelodhiambo84@gmail.com>
2026-06-17 08:00:23 +0300

    project initialization and set up

---
+++ src/main/java/com/manuelorg/cross_pesa/CrossPesaApplication.java
@@ -0,0 +1,13 @@
+package com.manuelorg.cross_pesa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossPesaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossPesaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manuelorg/cross_pesa/CrossPesaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manuelorg.cross_pesa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossPesaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
